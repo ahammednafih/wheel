d4331e0e800fd1877223386c6b6cdf8f98a19bb5
Ahammed Nafih <ahammednafihbk@gmail.com>
2022-02-17 21:52:09 +0530

    contacts listing page ui contacts listing table with pagination

---
+++ app/javascript/src/components/Dashboard/Contacts/Menubar/constants.js
@@ -0,0 +1,6 @@
+export const MENUBAR_MAIN_ITEMS = [
+  { label: "All", count: 0 },
+  { label: "Archived", count: 0 },
+  { label: "Completed", count: 0 },
+  { label: "Phase 2", count: 0 },
+];
---
+++ app/javascript/src/components/Dashboard/Contacts/Table.jsx
@@ -0,0 +1,63 @@
+import React, { useState, useMemo } from "react";
+
+import { MenuHorizontal } from "neetoicons";
+import { Table as NeetoUITable, Dropdown } from "neetoui";
+
+import { CONTACTS_TABLE_COLUMN_DATA, TABLE_PAGE_SIZE } from "./constants";
+import NameWithRole from "./NameWithRole";
+
+const nameWithRoleRenderer = (_, contact) => <NameWithRole contact={contact} />;
+
+const Table = ({ contacts = [], handleEdit, handleDelete }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const tableColumnData = useMemo(
+    () =>
+      CONTACTS_TABLE_COLUMN_DATA.map(columnData => {
+        if (columnData.key === "nameAndRole") {
+          return {
+            ...columnData,
+            render: nameWithRoleRenderer,
+          };
+        }
+
+        if (columnData.key === "actionButtons") {
+          return {
+            ...columnData,
+            render: (_, contact) => (
+              <Dropdown
+                icon={MenuHorizontal}
+                buttonStyle="text"
+                trigger="hover"
+              >
+                <li onClick={() => handleEdit(contact)}>Edit</li>
+                <li onClick={() => handleDelete(contact.id)}>Delete</li>
+              </Dropdown>
+            ),
+          };
+        }
+
+        return columnData;
+      }),
+    []
+  );
+
+  return (
+    <>
+      <div className="notes-table-height w-full">
+        <NeetoUITable
+          allowRowClick
+          fixedHeight
+          rowData={contacts}
+          columnData={tableColumnData}
+          currentPageNumber={currentPage}
+          handlePageChange={page => setCurrentPage(page)}
+          defaultPageSize={TABLE_PAGE_SIZE}
+          onRowClick={(_, contact) => handleEdit(contact)}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Table;
